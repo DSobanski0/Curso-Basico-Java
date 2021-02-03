3663d8dfe808ef820b1b2d0b386b747ce78dfc79
Daniel Sobanski <dsobanski0@gmail.com>
2021-02-03 15:32:38 -0300

    Add oriented object

---
+++ orientacao-objetos-java/src/main/java/oo/part02/construtores/exemplo002/Pessoa.java
@@ -0,0 +1,16 @@
+package oo.part02.construtores.exemplo002;
+
+public class Pessoa {
+    private String nome;
+
+    public Pessoa() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
